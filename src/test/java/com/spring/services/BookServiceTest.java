9d427addefbfabe6856bdb98bdb41b26861b625a
apokochito <diana.apolinar@hotmail.com>
2020-10-21 22:28:14 -0500

    Relocate test packages and add Service test

---
+++ src/test/java/com/spring/services/BookServiceTest.java
@@ -0,0 +1,13 @@
+package com.spring.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
